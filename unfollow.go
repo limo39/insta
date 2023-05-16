26bd9736c9be09f41312f19ab9703d7cbaecc935
LiMo Kiprono <94519855+limo39@users.noreply.github.com>
2023-05-16 22:14:59 +0300

    Update

--- unfollow.go
+++ unfollow.go
@@ -6,11 +6,22 @@ import (
 )
 
 func main() {
-    insta := goinsta.New("username", "password")
+    maxAttempts := 5
+    attempt := 0
 
-    if err := insta.Login(); err != nil {
-        panic(err)
+    for attempt < maxAttempts {
+        insta := goinsta.New("username", "password")
+
+        if err := insta.Login(); err == nil {
+            fmt.Println("Logged in as:", insta.Account.Username)
+            break
+        } else {
+            fmt.Println("Login failed. Retrying...")
+            attempt++
+        }
     }
 
-    fmt.Println("Logged in as:", insta.Account.Username)
+    if attempt == maxAttempts {
+        fmt.Println("Exceeded maximum login attempts. Exiting...")
+    }
 }
